cc8f3b750b27e43e882160082cfb0cc4ebf66a63
SamuelLepp <sleppiniemi@protonmail.com>
2024-10-10 12:15:19 +0300

    Docstrings

--- segmentqtl/fdr_correction.py
+++ segmentqtl/fdr_correction.py
@@ -5,7 +5,17 @@ import pandas as pd
 from statsmodels.stats.multitest import multipletests
 
 
-def combine_chromosome(outdir):
+def combine_chromosome(outdir: str):
+    """
+    Combine all csv files fro the given directory. In addition, save
+    the combined results as a csv file to the same location.
+
+    Parameters:
+    - outdir: Directory to which the mapping results have been saved.
+
+    Returns:
+    - combined_df: Dataframe with data from all csv files from the folder.
+    """
     csv_files = glob.glob(os.path.join(outdir, "*.csv"))
 
     dfs = []
@@ -22,7 +32,17 @@ def combine_chromosome(outdir):
     return combined_df
 
 
-def fdr(outdir, threshold):
+def fdr(outdir: str, threshold: float):
+    """
+    Perform Benjamini Hochberg false discovery rate correction to mapping results.
+
+    Parameters:
+    - outdir: Directory to which the mapping results have been saved.
+    - threshold: Cutoff value for fdr correction.
+
+    Returns:
+    - full_res: Dataframe with all mapping results including a column for fdr corrected p-value.
+    """
     full_res = combine_chromosome(outdir)
     perm_pvals = full_res["p_adj"]
     _, bh_p_values, _, _ = multipletests(perm_pvals, method="fdr_bh", alpha=threshold)
